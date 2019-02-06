5be87e82527b80cb36245c8c710f22d83663bbb0
frublev <46965588+frublev@users.noreply.github.com>
2019-02-06 21:47:49 +0300

    Update HomeWork 2.4.py

--- HomeWork 2.4.py
+++ HomeWork 2.4.py
@@ -5,7 +5,7 @@ def cookbook_load(cookbook_name):
             dishes = line.strip()
             cook_dict[dishes] = list()
             ingr_num = int(f_cook_book.readline())
-            for i in range (ingr_num):
+            for i in range(ingr_num):
                 ingedients_dict = {}
                 ingredient = f_cook_book.readline().split(" | ")
                 ingedients_dict['ingridient_name'] = ingredient[0]
@@ -15,6 +15,7 @@ def cookbook_load(cookbook_name):
             f_cook_book.readline()
     return cook_dict
 
+
 def get_shop_list_by_dishes(dishes, person_count):
     cook_dict = cookbook_load("cookbook.txt")
     shop_list = {}
@@ -23,7 +24,8 @@ def get_shop_list_by_dishes(dishes, person_count):
             if ingredient["ingridient_name"] in shop_list:
                 shop_list[ingredient["ingridient_name"]]["quantity"] += ingredient["quantity"] * person_count
             else:
-                shop_list[ingredient["ingridient_name"]] = {'measure': ingredient['measure'], 'quantity' : ingredient['quantity'] * person_count}
+                shop_list[ingredient["ingridient_name"]] = {'measure': ingredient['measure'], 'quantity': ingredient['quantity'] * person_count}
     print(shop_list)
+
 
 get_shop_list_by_dishes(["Омлет", "Фахитос"], 10)
