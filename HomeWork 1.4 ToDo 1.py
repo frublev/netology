5ef8e623aa9115239ec4cd3d9d14e8c6eb591d75
frublev <46965588+frublev@users.noreply.github.com>
2019-03-19 23:30:43 +0300

    Update HomeWork 1.4 ToDo 1.py

--- HomeWork 1.4 ToDo 1.py
+++ HomeWork 1.4 ToDo 1.py
@@ -1,21 +1,39 @@
-def adv_print(str, start="\n", max_line = False, in_file = False):
+def adv_print(*args, start="Пустая строка \n", max_line=False, in_file=False, separator=' '):
     print(start, end="")
-    if len(str) > max_line > 0:
-        line = []
-        while len(str) > max_line:
-            line.append(str[:max_line])
-            print(str[:max_line])
-            str = str[max_line:]
-        line.append(str)
-        print(str)
-        str = "\n".join(line)
+    output_line = []
+    last_str = 0
+    if separator == '\n':
+        end_print = separator
+        end_line = ''
     else:
-        print(str)
-    if in_file != False:
+        end_print = ''
+        end_line = separator
+    for arg in args:
+        output_text = str(arg) + end_line
+        line = []
+        if last_str > 0 and separator != '\n':
+            print(output_text[:last_str])
+            line.append(output_text[:last_str])
+            output_text = output_text[last_str:]
+        while len(output_text) > max_line:
+            line.append(output_text[:max_line])
+            print(output_text[:max_line])
+            output_text = output_text[max_line:]
+        last_str = max_line - len(output_text)
+        if last_str == 0:
+            print(output_text, end='\n')
+            line.append(output_text + '\n')
+            output_text = "\n".join(line)
+        else:
+            print(output_text, end=end_print)
+            line.append(output_text)
+            output_text = "\n".join(line) + end_print
+        output_line.append(output_text)
+    if in_file:
         with open(in_file, "w", encoding="utf-8") as file_for_write:
-            file_for_write.write(start+str)
+            file_for_write.write(start+''.join(output_line))
 
 
 if __name__ == "__main__":
-    str = "OneTwoThreeFourFiveS"
-    adv_print(str, max_line = 3, in_file = "text1.txt")
+    strk = "OneTwoThreeFourFiveSix"
+    adv_print(strk, 12345, 'test', 54321, 'test123', max_line=5, in_file="text2.txt", separator='\n')
