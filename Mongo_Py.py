b4970217b7d7be74a8aaef56697f83e8a550e225
frublev <46965588+frublev@users.noreply.github.com>
2019-04-08 15:42:57 +0300

    Create Mongo_Py.py

---
+++ Mongo_Py.py
@@ -0,0 +1,55 @@
+import csv
+import re
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+artist_db = client['artist']
+tickets = artist_db['tickets']
+
+def read_data(csv_file, db):
+    """
+    Загрузить данные в бд из CSV-файла
+    """
+    data = []
+    with open(csv_file, encoding='utf8') as csvfile:
+        # прочитать файл с данными и записать в коллекцию
+        reader = csv.reader(csvfile)
+        reader = list(reader)
+        header = reader.pop(0)
+        for line in reader:
+            artist, price, place, date = line
+            price = int(price)
+            data.append({
+                'artist': artist,
+                'price': price,
+                'place': place,
+                'date': date
+            })
+    artist_db.tickets.insert_many(data)
+
+
+def find_cheapest(db, count):
+    """
+    Найти самые дешевые билеты
+    Документация: https://docs.mongodb.com/manual/reference/operator/aggregation/sort/
+    """
+    return db.find().sort([('price', 1)]).limit(count)
+
+
+def find_by_name(name, db):
+    """
+    Найти билеты по имени исполнителя (в том числе – по подстроке),
+    и выведите их по возрастанию цены
+    """
+    regex = re.compile(name + '+', re.IGNORECASE)
+    return db.find({'artist': regex}).sort([('price', pymongo.ASCENDING)])
+
+
+if __name__ == '__main__':
+    read_data('artist.csv', 'tickets_db')
+    a = find_cheapest(artist_db.tickets, 3)
+    print(list(a))
+    b = find_by_name('ов', artist_db.tickets)
+    print(list(b))
+
