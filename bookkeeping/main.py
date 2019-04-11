5eac99ff177bbdb86af912c2fc4772e1ced89324
frublev <46965588+frublev@users.noreply.github.com>
2019-04-11 07:54:50 +0300

    HW1

---
+++ bookkeeping/main.py
@@ -0,0 +1,6 @@
+from bookkeeping.application.db.people import get_employees
+from bookkeeping.application.salary import calculate_salary
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
