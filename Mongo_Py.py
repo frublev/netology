56af502fb5c597246dba2f203b6d5303b17cc086
frublev <46965588+frublev@users.noreply.github.com>
2019-04-17 12:49:29 +0300

    Update Mongo_Py.py

--- Mongo_Py.py
+++ Mongo_Py.py
@@ -15,8 +15,7 @@ def read_data(csv_file, db):
     with open(csv_file, encoding='utf8') as csvfile:
         # прочитать файл с данными и записать в коллекцию
         reader = csv.reader(csvfile)
-        reader = list(reader)
-        header = reader.pop(0)
+        next(reader)
         for line in reader:
             artist, price, place, date = line
             price = int(price)
@@ -26,7 +25,7 @@ def read_data(csv_file, db):
                 'place': place,
                 'date': date
             })
-    artist_db.tickets.insert_many(data)
+    db.insert_many(data)
 
 
 def find_cheapest(db, count):
@@ -42,14 +41,13 @@ def find_by_name(name, db):
     Найти билеты по имени исполнителя (в том числе – по подстроке),
     и выведите их по возрастанию цены
     """
-    regex = re.compile(name + '+', re.IGNORECASE)
+    regex = re.compile(re.escape(name), re.IGNORECASE)
     return db.find({'artist': regex}).sort([('price', pymongo.ASCENDING)])
 
 
 if __name__ == '__main__':
-    read_data('artist.csv', 'tickets_db')
+    read_data('artist.csv', tickets)
     a = find_cheapest(artist_db.tickets, 3)
     print(list(a))
-    b = find_by_name('ов', artist_db.tickets)
+    b = find_by_name('.', artist_db.tickets)
     print(list(b))
-
